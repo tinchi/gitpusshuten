e1e0ffe958f30f3afe662657e30ea329d577d92b
Michael van Rooijen <meskyanichi@gmail.com>
2010-11-24 00:17:43 +0100

    Finished building the new Active Record CLI and re-wrote the Active Record hooks file to work with it.

--- lib/gitpusshuten/modules/active_record/command.rb
+++ lib/gitpusshuten/modules/active_record/command.rb
@@ -2,11 +2,14 @@
 module GitPusshuTen
   module Commands
     class ActiveRecord < GitPusshuTen::Commands::Base
-      description "[Module] Redis commands."
-      usage       "redis <command> for <enviroment>"
-      example     "heavenly redis install                 # Installs Redis (system wide) and downloads config template."
-      example     "heavenly redis upload-configuration    # Uploads the Redis configuration template to the server, will install Redis if not already present."
-      example     "               upload-config           # Alias."
+      description "[Module] Active Record commands."
+      usage       "active_record <command> for <enviroment>"
+      example     "heavenly active_record create_configuration    # Creates a template for your remote database."
+      example     "                       create_config           # Alias."  
+      example     "heavenly active_record upload_configuration    # Uploads your configuration file to the remote server."
+      example     "                       upload_config           # Alias."
+      example     "heavenly active_record download_configuration  # Downloads your configuration file from the remote server."
+      example     "                       download_config         # Alias."
 
       def initialize(*objects)
         super
@@ -22,8 +25,10 @@ module GitPusshuTen
         
         ##
         # Default Configuration
-        @local_configuration_dir  = File.join(local.gitpusshuten_dir, 'active_record')
-        @local_configuration_file = File.join(@local_configuration_dir, "#{e.name}.database.yml")
+        @local_configuration_dir    = File.join(local.gitpusshuten_dir, 'active_record')
+        @local_configuration_file   = File.join(@local_configuration_dir, "#{e.name}.database.yml")
+        @remote_configuration_dir   = File.join(c.path, 'modules', 'active_record')
+        @remote_configuration_file  = File.join(@remote_configuration_dir, "#{e.sanitized_app_name}.#{e.name}.database.yml")
         
         ##
         # Ensure the directory is always available
@@ -33,31 +38,72 @@ module GitPusshuTen
       ##
       # Creates a template for the user to work with
       def perform_create_configuration!
-        if File.exist?(@local_configuration_file)
-          warning "Configuration file already exists in #{@local_configuration_file}."
-          warning "Would you like to overwrite?"
-          exit unless yes?
-        end
+        overwrite_if_exists?
         
         render_database_yml
+        message "Created #{y(@local_configuration_file)}."
       end
 
+      ##
+      # Alias to perform_create_configuration!
       def perform_create_config!
         perform_create_configuration!
       end
 
+      ##
+      # Uploads the local configuration file to the remote server.
       def perform_upload_configuration!
+        requires_user_existence!
+        prompt_for_user_password!
         
+        if not File.exist?(@local_configuration_file)
+          error "Could not find #{y(@local_configuration_file)}."
+          error "Either download an existing one from your server with:"
+          standard "\n\s\s#{y("heavenly active_record download-configuration from #{e.name}")}\n\n"
+          error "Or create a new template with:"
+          standard "\n\s\s#{y("heavenly active_record create-configuration for #{e.name}")}"
+          exit
+        end
+        
+        ##
+        # Ensure the remote active_record module directory exits
+        # and upload the local file to the remote file location
+        Spinner.return :message => "Uploading the database configuration file.." do
+          e.execute_as_user("mkdir -p '#{@remote_configuration_dir}'")
+          e.scp_as_user(:upload, @local_configuration_file, @remote_configuration_file)
+          g('Done!')
+        end
       end
 
+      ##
+      # Alias to perform_upload_configuration!
       def perform_upload_config!
         perform_upload_configuration!
       end
 
+      ##
+      # Downloads the configuration file from the remote server.
       def perform_download_configuration!
+        overwrite_if_exists?
         
+        ##
+        # Don't proceed unless the file exists
+        if not e.file?(@remote_configuration_file)
+          error "Configuration could not be found on the server in #{y(@remote_configuration_file)}."
+          exit
+        end
+        
+        ##
+        # Ensure the remote active_record module directory exits
+        # and upload the local file to the remote file location
+        Spinner.return :message => "Downloading the database configuration file.." do
+          e.scp_as_user(:download, @remote_configuration_file, @local_configuration_file)
+          g('Done!')
+        end        
       end
 
+      ##
+      # Alias to perform_download_configuration!
       def perform_download_config!
         perform_download_configuration!
       end
@@ -70,9 +116,14 @@ module GitPusshuTen
           menu.choice('mysql', 'mysql2')
           menu.choice('postgresql')
           menu.choice('sqlite3')
+          menu.choice('oracle')
+          menu.choice('frontbase')
+          menu.choice('ibm_db')
         end
       end
 
+      ##
+      # Render the chosen Active Record database template file 
       def render_database_yml
         config_content = case choose_adapter
         when 'mysql', 'mysql2'
@@ -81,19 +132,19 @@ production:
   adapter: mysql2
   encoding: utf8
   reconnect: false
-  database: #{c.application}.#{e.name}
+  database: #{e.sanitized_app_name}_#{e.name}
   pool: 5
   username: #{c.user}
   password:
   host: localhost
-  socket: /tmp/mysql.sock
+  # socket: /tmp/mysql.sock
 CONFIG
         when 'postgresql'
 <<-CONFIG
 production:
   adapter: postgresql
   encoding: unicode
-  database: #{c.application}.#{e.name}
+  database: #{e.sanitized_app_name}_#{e.name}
   pool: 5
   username: #{c.user}
   password:
@@ -110,7 +161,7 @@ CONFIG
 <<-CONFIG
 production:
   adapter: oracle
-  database: #{c.application}.#{e.name}
+  database: #{e.sanitized_app_name}_#{e.name}
   username: #{c.user}
   password: 
 CONFIG
@@ -119,7 +170,7 @@ CONFIG
 production:
   adapter: frontbase
   host: localhost
-  database: #{c.application}.#{e.name}
+  database: #{e.sanitized_app_name}_#{e.name}
   username: #{c.user}
   password: 
 CONFIG
@@ -129,7 +180,7 @@ production:
   adapter: ibm_db
   username: #{c.user}
   password:
-  database: #{c.application}.#{e.name}
+  database: #{e.sanitized_app_name}_#{e.name}
   #schema: db2inst1
   #host: localhost
   #port: 50000
@@ -146,6 +197,17 @@ CONFIG
         
         File.open(@local_configuration_file, 'w') do |file|
           file << config_content
+        end
+      end
+
+      ##
+      # If the local configuration file exists, it will ask the
+      # user whether he wants to overwrite it
+      def overwrite_if_exists?
+        if File.exist?(@local_configuration_file)
+          warning "Configuration file already exists in #{@local_configuration_file}."
+          warning "Would you like to overwrite?"
+          exit unless yes?
         end
       end
 
