310a869756910daf631297a6fadc77001503d8e9
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2010-12-13 18:19:53 +0530

    Add support for ansicon and win32console gem for colors support in Windows shell.

--- lib/gitpusshuten.rb
+++ lib/gitpusshuten.rb
@@ -11,6 +11,14 @@ require 'highline/import'
 require 'rainbow'
 require 'json'
 
+if Config::CONFIG['host_os'] =~ /mswin|mingw/
+  begin
+    require 'win32console'
+  rescue LoadError
+    puts "You must install 'win32console' gem or use ANSICON 1.31 or later (http://adoxa.110mb.com/ansicon/) to use color on Windows"
+  end unless ENV['ANSICON']
+end
+
 Dir[File.expand_path(File.join(File.dirname(__FILE__), 'gitpusshuten/**/*'))].each do |file|
   if not File.directory?(file) and not file =~ /\/modules\/.+\/hooks\.rb/
     require file
