a45df531e3da798123e50a26b10050d39b334e11
Andrey Lompart <andrey.lompart@gmail.com>
2013-02-16 17:14:11 +0200

    fix log tests

--- spec/log_spec.rb
+++ spec/log_spec.rb
@@ -1,20 +1,18 @@
-require 'spec_helper'
+# require 'spec_helper'
 
 describe GitPusshuTen::Log do
-  
   it "should log a message" do
-    GitPusshuTen::Log.expects(:puts).with("[message] heavenly message")
+    GitPusshuTen::Log.expects(:puts).with("[message] ".color(:green) + "heavenly message")
     GitPusshuTen::Log.message("heavenly message")
   end
   
   it "should log a message" do
-    GitPusshuTen::Log.expects(:puts).with("[warning] heavenly message")
+    GitPusshuTen::Log.expects(:puts).with("[warning] ".color(:yellow) + "heavenly message")
     GitPusshuTen::Log.warning("heavenly message")
   end
   
   it "should log a message" do
-    GitPusshuTen::Log.expects(:puts).with("[error] heavenly message")
+    GitPusshuTen::Log.expects(:puts).with("[error] ".color(:red) + "heavenly message")
     GitPusshuTen::Log.error("heavenly message")
   end
-  
 end
